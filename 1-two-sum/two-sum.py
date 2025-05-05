68edcd68c9d6dd51ab8968e2ceab57c5e32a79bd
Hoothoot <hop2work@gmail.com>
2025-05-05 16:48:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (11.18%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        difference_tracker = {}
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in difference_tracker:
+                # we have found the solution
+                return [difference_tracker[diff], i]
+            difference_tracker[nums[i]] = i
+
+
+
+
+        
